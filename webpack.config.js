0e8c545ec5c2aa2afbec1b496be438a4539e26b2
jiazhiteng <jiazhiteng@lexue.com>
2020-09-23 21:55:28 +0800

    项目初始化

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: jiazhiteng@lexue.com
+ * @Date: 2020-09-23 20:54:12
+ * @LastEditTime: 2020-09-23 21:26:32
+ * @LastEditors: JiaZhiteng
+ * @Description: webpack入口文件
+ * @FilePath: /toy-react/webpack.config.js
+ */
+
+module.exports = {
+  entry: {
+    main: './main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [['@babel/plugin-transform-react-jsx', {praagma: 'createElement'}]]
+          }
+        }
+      }
+    ]
+  },
+  mode: "development", // 使人类易读懂的打包后代码
+  optimization: {
+    minimize: false
+  }
+}
